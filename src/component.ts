d6840c42310859f13f81e98bacbbe226b1ed09bb
alejandroileal <aleal1202@outlook.com>
2023-10-27 22:16:58 +0200

    Setup files and add component

---
+++ src/component.ts
@@ -0,0 +1,22 @@
+export abstract class Component {
+  selector: string;
+  template!: string;
+  element!: Element;
+  constructor(selector: string) {
+    this.selector = selector;
+  }
+
+  render(position: InsertPosition = 'beforeend') {
+    const element = document.querySelector(this.selector);
+    if (!element) {
+      return;
+    }
+
+    element.insertAdjacentHTML(position, this.template);
+    this.element = element.lastElementChild!;
+  }
+
+  clear() {
+    this.element.outerHTML = '';
+  }
+}
