588a78a5f34ae97d7656208da512c021584f928d
alejandroileal <aleal1202@outlook.com>
2023-10-28 00:45:51 +0200

    Add main ts file

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
-const main = () => {};
+import { Header } from './components/header';
+import { Menu } from './components/menu';
+import { MenuOption } from './types';
 
 console.log('Loaded Main 😺');
 
-console.log('Connected');
-
-main();
+const menu: MenuOption[] = [
+  { label: 'Pokemons', path: 'index.html' },
+  { label: 'My Pokemons', path: 'library.html' },
+];
+const renderizedContent = [new Header('#app', menu)];
